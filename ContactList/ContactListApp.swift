535cbf23291fc5694d499fecb32d096581545f12
Пахаев Хусейн <rvoltigo@gmail.com>
2023-06-24 12:45:34 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Khusain on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
